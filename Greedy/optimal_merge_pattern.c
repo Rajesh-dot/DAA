3498912e049228e158488d491eedd82c2c224af8
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2022-03-07 21:08:44 +0530

    Update optimal_merge_pattern.c

--- Greedy/optimal_merge_pattern.c
+++ Greedy/optimal_merge_pattern.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 
-int get_min(int a[],int n){
-    int idx=-1;
-    int m=1000;
-    for(int i=0;i<n;i++){
-        if(m>a[i]){
-            idx=i;
-            m=a[i];
-        }
+void heapify(int arr[],int n,int i){
+    int largest=i;
+    int l=2*i+1;
+    int r=2*i+2;
+    int t;
+    if(l<n && arr[l]<arr[largest])
+        largest=l;
+    if(r<n && arr[r]<arr[largest])
+        largest=r;
+    if(largest!=i){
+        t=arr[i];
+        arr[i]=arr[largest];
+        arr[largest]=t;
+        heapify(arr, n, largest);
     }
-    return idx;
 }
 
+
 int main(){
-    int n,idx1,idx2,min1,min2;
+    int n,min1,min2;
     printf("Enter number of files: ");
     scanf("%d",&n);
     int f[n];
@@ -21,14 +27,17 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&f[i]);
     }
+    for(int i=n/2-1;i>=0;i--){
+        heapify(f,n,i);
+    }
     int cost=0;
     for(int i=0;i<n-1;i++){
-        idx1=get_min(f,n);
-        min1=f[idx1];
-        f[idx1]=1000;
-        idx2=get_min(f,n);
-        min2=f[idx2];
-        f[idx2]=min1+min2;
+        min1=f[0];
+        f[0]=1000;
+        heapify(f,n,0);
+        min2=f[0];
+        f[0]=min1+min2;
+        heapify(f,n,0);
         cost=cost+min1+min2;
         printf("merge %d and %d cost is = %d\n",min1,min2,min1+min2);
     }
