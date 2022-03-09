08e2769c7700fd6331f8488370c4e566a9b4d3ca
Rajesh-dot <57223067+Rajesh-dot@users.noreply.github.com>
2022-03-10 01:01:24 +0530

    added outputs

--- Greedy/Knapsack_problem.c
+++ Greedy/Knapsack_problem.c
@@ -1,36 +1,3 @@
-/*
-Problem:- Knapsack Problem (Greedy Approach)
-Author:- The_King
-
-Input Format:-
-
-    .The input contains of 3 lines
-
-    .The first line of input contains two integers n and w representing the number of items and max weight the bag can hold
-    .The second line contains n space separated integers where i-th number represent the weight of the i-th bag
-    .The third line contains n space separated integers where i-th number represent the value of the i-th bag
-
-Output Format:-
-
-    .The output contains of 2 lines
-
-    .The first line contains a single Integer representing th maximum profit
-    .The second line contains n space separated numbers where i-th number represent the amount of the i-th item taken
-
-Sample input:-
-    3 20
-    18 15 10
-    25 20 15
-
-sample output:-
-    28.888889
-    1.000000 0.555556 0.000000
-
-*/
-
-
-//Code:-
-
 #include<stdio.h>
 
 int main(){
