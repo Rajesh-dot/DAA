0e9433c416c83e6371fb08a6550a3aba1e81c689
Kaipa Rajeswara Reddy <57223067+Rajesh-dot@users.noreply.github.com>
2022-08-21 15:40:02 +0530

    Create Knapsack_problem.py

---
+++ Dynamic Programming/Knapsack_problem.py
@@ -0,0 +1,12 @@
+def knapSack(W, wt, val, n):
+  dp = [0 for i in range(W+1)]
+	for i in range(1, n+1):
+		for w in range(W, 0, -1):
+			if wt[i-1] <= w:
+				dp[w] = max(dp[w], dp[w-wt[i-1]]+val[i-1])
+	return dp[W]
+
+n,k=map(int,input().split())
+wt=list(map(int,input().split()))
+val=list(map(int,input().split()))
+print(knapSack(k,wt,val,n))
